c8dbfcbe4d61d0d932c0c88ed1a455155db1eb4e
shreyapande101 <shreyapande101@gmail.com>
2026-02-14 18:21:48 +0530

    done

---
+++ readme.md
@@ -0,0 +1,32 @@
+#  Create Your Notes 
+
+This is a simple **Notes Web Application** made using **HTML, CSS, and JavaScript**.  
+It allows users to write, save, and delete notes easily in a clean interface.
+
+
+---
+
+ Features
+
+- Create and write notes
+- Delete notes easily
+- Beginner-friendly project
+
+---
+  Language Used
+
+- **HTML** – for structure  
+- **CSS** – for styling and gradient design  
+- **JavaScript** – for creating and deleting notes  
+
+---
+
+ How to Use
+
+1. Click on the **Create Notes** button  
+2. Write your notes in the text area  
+3. Notes can be edited anytime  
+4. Click the **Delete** icon to remove a note 
+
+
+
--- script.js
+++ script.js
@@ -2,6 +2,15 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
+function showNotes(){
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+showNotes();
+
+function updateStorage(){
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
 
 createBtn.addEventListener("click", ()=>{
     let inputBox = document.createElement("p");
@@ -17,6 +26,23 @@ notesContainer.addEventListener("click", function(e){
     if(e.target.tagName === "IMG"){
         
         e.target.parentElement.remove();
+        updateStorage();   
 
+    }
+     else if(e.target.tagName === "P"){
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+            nt.onkeyup = function(){
+                updateStorage();
+            }
+        })
+     }
+
+})
+
+document.addEventListener("keydown", event =>{
+    if(event.key === "Enter"){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
     }
 })
